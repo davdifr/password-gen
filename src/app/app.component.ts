9bc35f9845e8dd302cc5105f090ae1ef7fa86dc3
Davide Di Francesco <davdifr@outlook.com>
2024-05-12 02:03:17 +0200

    chore: Add Tailwind CSS configuration and update dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,53 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PasswordService } from './password.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'password-gen';
+
+  password = '';
+  buttonText = 'Copy Password';
+
+  length = 24;
+  numbers = false;
+  symbols = false;
+  lowercase = true;
+  uppercase = false;
+
+  constructor(private passwordService: PasswordService) {}
+
+  onGeneratePasswordClick() {
+    try {
+      this.password = this.passwordService.generatePassword(
+        this.length,
+        this.numbers,
+        this.symbols,
+        this.lowercase,
+        this.uppercase
+      );
+
+      this.buttonText = 'Copy Password';
+    } catch (error) {
+      // Display error to the user
+      alert(
+        'An error occurred while generating the password. Please try again.'
+      );
+    }
+  }
+
+  onCopyPasswordClick() {
+    navigator.clipboard.writeText(this.password);
+    this.buttonText = 'Copied!';
+
+    // Reset button text after 3 seconds
+    setTimeout(() => {
+      this.buttonText = 'Copy Password';
+    }, 3000);
+  }
 }
