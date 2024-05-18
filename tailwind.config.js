a301199f833c2d00c5abdb3b04748a520851e034
Antonio Mario Cavuto <tony@Antonios-MBP.homenet.telecomitalia.it>
2024-05-18 18:29:13 +0200

    prettier sorting plugin + tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    colors: {
+      base: {
+        100: "hsl(var(--base-100) / <alpha-value>)",
+        200: "hsl(var(--base-200) / <alpha-value>)",
+        300: "hsl(var(--base-300) / <alpha-value>)",
+        400: "hsl(var(--base-400) / <alpha-value>)",
+        500: "hsl(var(--base-500) / <alpha-value>)",
+        600: "hsl(var(--base-600) / <alpha-value>)",
+        900: "hsl(var(--base-900) / <alpha-value>)",
+      },
+      primary: "hsl(var(--primary) / <alpha-value>)",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/forms")],
